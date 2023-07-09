3ae513922a108cd5e6c74f442f191b812bb1a95a
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2023-07-09 02:49:43 +0200

    feat: implemented a default layout and the main index page

---
+++ frontend/layout/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-app-bar app color="black" height="100">
+      <v-container>
+        <v-row align="center">
+          <v-app-bar-title>
+            <v-img src="/img/logo.ico" height="80" width="80" @click="$router.push('/')" />
+          </v-app-bar-title>
+          <v-list color="black" style="color: black !important" class="d-flex align-center">
+            <v-list-item v-for="(item, index) in menus" :key="index" color="black" list>
+              <v-list-item-title>
+                <NuxtLink :to="`${item.route}`">
+                  <v-btn color="white">{{ item.title }}</v-btn>
+                </NuxtLink>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+          <v-spacer></v-spacer>
+          <v-btn variant="outlined" color="white">Kyqu</v-btn>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+const menus = [
+  { title: 'Ballina', route: '/' },
+  { title: 'Serviset', route: '/serviset' },
+  { title: 'Kontakto', route: '/kontakto' },
+];
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app>
+    <v-carousel cycle height="700" show-arrows hide-delimiter-background>
+      <template #prev="{ props }">
+        <v-btn class="ma-2" variant="text" icon="mdi-chevron-left" @click="props.onClick" />
+      </template>
+      <template #next="{ props }">
+        <v-btn class="ma-2" variant="text" icon="mdi-chevron-right" @click="props.onClick" />
+      </template>
+      <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slide.src" cover />
+    </v-carousel>
+    <v-sheet style="margin: 0% 15% 0% 15%; padding: 0% 1% 0% 1%">
+      <v-timeline truncate-line="end">
+        <v-timeline-item v-for="(item, i) in items" :key="i">
+          <template #opposite>
+            <v-img :src="item.image" />
+          </template>
+          <div>
+            <p class="text-h6" v-text="item.label" />
+            <p v-text="item.description" />
+          </div>
+        </v-timeline-item>
+      </v-timeline>
+    </v-sheet>
+  </v-app>
+</template>
+
+<script setup>
+const slides = [
+  {
+    src: '/img/C1.jpg',
+  },
+  {
+    src: '/img/C2.jpg',
+  },
+  {
+    src: '/img/C3.jpg',
+  },
+  {
+    src: '/img/C4.jpg',
+  },
+  {
+    src: '/img/C5.jpg',
+  },
+];
+
+const items = ref([
+  {
+    label: 'Banese',
+    image: '/img/P1.jpg',
+    description: 'Banese ne shitje',
+  },
+  {
+    label: 'Shpi',
+    image: '/img/P2.jpg',
+    description: 'Shpi ne shitje',
+  },
+  {
+    label: 'Lokal',
+    image: '/img/P3.jpg',
+    description: 'Lokal ne shitje',
+  },
+  {
+    label: 'Toke',
+    image: '/img/P4.jpg',
+    description: 'Toke ne shitje',
+  },
+  {
+    label: 'Prone',
+    image: '/img/P5.jpg',
+    description: 'Prone ne shitje',
+  },
+]);
+</script>
+
+<style>
+.v-img__img {
+  position: relative !important;
+}
+</style>
